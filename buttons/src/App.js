4748f216ad2e78c81685c8029867c975a7c8a244
Ryusuke24 <yunglean_24@mail.ru>
2023-11-29 15:17:04 +0300

    page110

--- buttons/src/App.js
+++ buttons/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Buttons from "./components/Buttons";
+import appStyle from "./App.module.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className={appStyle.app}>
+        <h2 className={appStyle.title}>CSS Module Buttons</h2>
+      </div>
+      <div className={appStyle.wrapper}>
+        <Buttons />
+      </div>
+    </>
   );
 }
 
---
+++ buttons/src/components/Buttons.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Buttons.module.css";
+
+const Buttons = () => (
+  <>
+    <button className={styles.btn1}>press1</button>
+    <button className={styles.btn2}>press2</button>
+    <button className={styles.btn3}>press3</button>
+  </>
+);
+
+export default Buttons;
